d8c15f9a8d42867ef05d98657469a46669cba8fb
techmaster <techmaster@techmasters-iMac-3.local>
2017-01-07 17:07:07 +0700

    First commit

--- DrawLedBall/ViewController.swift
+++ DrawLedBall/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfNumber: UITextField!
+    
+    var ball = UIImageView()
+    var viewShow = UIView()
+    let marginTop: CGFloat = 70.0
+    var marginLeading: CGFloat = 50.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tfNumber.text = String(0)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setBall() {
+        ball = UIImageView(image: UIImage(named: "green"))
+        viewShow.addSubview(ball)
     }
-
-
+    
+    @IBAction func btnDraw(_ sender: UIButton) {
+        viewShow.removeFromSuperview()
+        viewShow = UIView()
+        view.addSubview(viewShow)
+        let number: Int = (Int(tfNumber.text!))!
+        if number >= 2{
+            for index1 in 0..<number {
+                for index2 in 0..<number {
+                    setBall()
+                    ball.center = CGPoint(x: marginLeading + spaceBetweenCol()*CGFloat(index2)/CGFloat(number - 1), y: marginTop + spaceBetweenRow()*CGFloat(index1)/CGFloat(number - 1))
+                }
+            }
+        } else if number == 1 {
+            setBall()
+            ball.center = view.center
+        }
+    }
+    
+    func spaceBetweenCol() -> CGFloat {
+        return view.bounds.size.width - 2*marginLeading
+    }
+    
+    func spaceBetweenRow() -> CGFloat {
+        return view.bounds.size.height - 2*marginTop
+    }
 }
 
